d2ebe15bf3933da8a86bd278a5795f1d5e68c82d
Milans Thapa <milan0.thapa1@gmail.com>
2013-02-14 21:27:15 +0545

    wao group number could also be specified

--- main.sh
+++ main.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+PRG=$(basename $0)
+
+function Usage {
+    echo -e "Usage: \t$PRG gpNo";
+    echo -e "\t gpNo --your loadshedding group number...\nEg @ku ko gpNo:7 so type \n ./$PRG 7"
+}
+
+# checking arguments
+if [ $# -eq 0 ]; then
+    Usage;
+    exit 1;
+fi
+
+
 #first download a fresh file
 #bash ./downloader.sh
 
@@ -8,3 +22,9 @@ bash ./convertor.sh
 
 #extract out the schedule
 bash ./extract.sh
+
+#output schedule as desired group number
+bash ./feeder.sh $1
+
+
+
